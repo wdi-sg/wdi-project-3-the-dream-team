3528f560dffc44d6754e19665f4d05315f01d8d4
Raymond Aung <raymond.psaung@gmail.com>
2017-05-03 16:50:43 +0800

    devise default setup with User model

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+# devise for user authentication and registration
+gem 'devise'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
-  root 'dummy_page#index'
-  get 'dummy_page/index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  root 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
