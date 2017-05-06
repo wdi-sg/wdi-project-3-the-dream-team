02b6f701c2cb0147b724bad605189ed83fe127ba
Shirong Foo <dee.sr.foo@gmail.com>
2017-05-06 19:27:58 +0800

    calendar

--- db/migrate/20170502004101_create_sessions.rb
+++ db/migrate/20170502004101_create_sessions.rb
@@ -2,8 +2,8 @@ class CreateSessions < ActiveRecord::Migration[5.0]
   def change
     create_table :sessions do |t|
       t.belongs_to :event
-      t.date :datetime_from
-      t.date :datetime_to
+      t.datetime :datetime_from
+      t.datetime :datetime_to
       t.integer :price
       t.integer :capacity
       t.integer :pax
